d10c816ccaf645ff5d755de823932f36127361ea
riscuus <riscoalbert@gmail.com>
2022-02-27 18:37:08 -0500

    Changed tabs for spaces

--- sim/TB/Top_TB.vhd
+++ sim/TB/Top_TB.vhd
@@ -100,7 +100,7 @@ signal LD_IO13: std_logic; -- D->A, time critical as it defines the new pixel
 signal CK_IO27: std_logic; -- D->A, Chip Select active low
 signal CS_IO26: std_logic; -- D->A Chip Serial Clock for the channel D->A active low
 signal SDI_IO28: std_logic; -- D->A Data
-		
+        
 signal START_CONV_ADC_CH_PULSE_signal  : std_logic;
 signal START_CONV_DAC_CH_PULSE_signal  : std_logic;
 
@@ -114,62 +114,62 @@ begin
 
     top_entity : entity concept.top_entity
     generic map(
-				COLUMS_NUM  => COLUMNS_NUM,
-				ROWS_NUM  => ROWS_NUM
-			)
+                COLUMS_NUM  => COLUMNS_NUM,
+                ROWS_NUM  => ROWS_NUM
+            )
     Port map (
         Sys_Clock => clk_signal_100mhz,
-	    Sys_Reset_in => rst_signal,
-	    row_sync  => col_sync_signal,
+        Sys_Reset_in => rst_signal,
+        row_sync  => col_sync_signal,
 
-		-- dummy ports for signals
-		
---		b_coefficients  =>  b_coefficients_signal,
---		a_coefficients  =>  a_coefficients_signal,
-		
-	    --- Channel Operations ---
-		CNV_IO0 =>   CNV_IO0_signal,
-		SCK_IO1 =>   SCK_IO1_signal,
-		SDO_CH =>  SDO_CH_signal,
-		
-		LD_IO3 =>    LD_IO3_signal,
-		CS_IO4 =>    CS_IO4_signal,
+        -- dummy ports for signals
+        
+        --b_coefficients  =>  b_coefficients_signal,
+        --a_coefficients  =>  a_coefficients_signal,
+        
+        --- Channel Operations ---
+        CNV_IO0 =>   CNV_IO0_signal,
+        SCK_IO1 =>   SCK_IO1_signal,
+        SDO_CH =>  SDO_CH_signal,
+        
+        LD_IO3 =>    LD_IO3_signal,
+        CS_IO4 =>    CS_IO4_signal,
         CK_IO5 =>	 CK_IO5_signal,
-		SDI_CH =>    SDI_CH_signal,
-		
-		--- Row Operations --- 
-		LD_IO7 =>    LD_IO7_signal,
-		CS_IO8 =>    CS_IO8_signal,
-		CS_IO9 =>    CS_IO9_signal,
-		CS_IO10 =>   CS_IO10_signal,
-		CK_IO11 =>   CK_IO11_signal,
-		SDI_IO12 =>  SDI_IO12_signal,
-		
-		--- Bias Operations ---
-		LD_IO13 =>   LD_IO13,
-		CK_IO27 =>   CK_IO27,
-		CS_IO26 =>   CS_IO26,
-		SDI_IO28 =>  SDI_IO28,
-		
-		START_CONV_ADC_CH_PULSE => START_CONV_ADC_CH_PULSE_signal,
-		START_CONV_DAC_CH_PULSE => START_CONV_DAC_CH_PULSE_signal,
-		
-		valid_DAC => valid_DAC_signal,
-		--DAC_data_Parallel => DAC_data_Parallel_signal,
-		
-	    sw => sw_signal,
-		LED => LED_signal
+        SDI_CH =>    SDI_CH_signal,
+        
+        --- Row Operations --- 
+        LD_IO7 =>    LD_IO7_signal,
+        CS_IO8 =>    CS_IO8_signal,
+        CS_IO9 =>    CS_IO9_signal,
+        CS_IO10 =>   CS_IO10_signal,
+        CK_IO11 =>   CK_IO11_signal,
+        SDI_IO12 =>  SDI_IO12_signal,
+        
+        --- Bias Operations ---
+        LD_IO13 =>   LD_IO13,
+        CK_IO27 =>   CK_IO27,
+        CS_IO26 =>   CS_IO26,
+        SDI_IO28 =>  SDI_IO28,
+        
+        START_CONV_ADC_CH_PULSE => START_CONV_ADC_CH_PULSE_signal,
+        START_CONV_DAC_CH_PULSE => START_CONV_DAC_CH_PULSE_signal,
+        
+        valid_DAC => valid_DAC_signal,
+        --DAC_data_Parallel => DAC_data_Parallel_signal,
+        
+        sw => sw_signal,
+        LED => LED_signal
     );
-		
+        
 
-	b_coefficients_signal(0)(15 downto 0) <= X"0000";
-	b_coefficients_signal(1)(15 downto 0) <= X"0000";
-	b_coefficients_signal(2)(15 downto 0) <= X"0000";
-	b_coefficients_signal(3)(15 downto 0) <= X"0000";
-	a_coefficients_signal(0)(15 downto 0) <= X"0000";
-	a_coefficients_signal(1)(15 downto 0) <= X"0000";
-	a_coefficients_signal(2)(15 downto 0) <= X"0000";
-	a_coefficients_signal(3)(15 downto 0) <= X"0000";
+    b_coefficients_signal(0)(15 downto 0) <= X"0000";
+    b_coefficients_signal(1)(15 downto 0) <= X"0000";
+    b_coefficients_signal(2)(15 downto 0) <= X"0000";
+    b_coefficients_signal(3)(15 downto 0) <= X"0000";
+    a_coefficients_signal(0)(15 downto 0) <= X"0000";
+    a_coefficients_signal(1)(15 downto 0) <= X"0000";
+    a_coefficients_signal(2)(15 downto 0) <= X"0000";
+    a_coefficients_signal(3)(15 downto 0) <= X"0000";
 
 -----------------------------------------------------------------------------------------------------------------------												
 -- Col Sync Signal PROCESS ------------------------------------------------------------------------------------------------------
@@ -178,17 +178,17 @@ begin
 data_in: PROCESS
 BEGIN
     col_sync_signal <= '0';
-	wait for Tclk_100mhz*123;
+    wait for Tclk_100mhz*123;
     
     for i in 1 to 10000 loop
-		col_sync_signal <= '1';
-		wait for Tclk_100mhz;
-		col_sync_signal <= '0';
-		wait for Tclk_100mhz*499;
-	end loop;
+        col_sync_signal <= '1';
+        wait for Tclk_100mhz;
+        col_sync_signal <= '0';
+        wait for Tclk_100mhz*499;
+    end loop;
     
-	wait for 2000 ms;
-	
+    wait for 2000 ms;
+    
 END PROCESS data_in;	
 
 -----------------------------------------------------------------------------------------------------------------------
@@ -202,108 +202,63 @@ BEGIN
     for i in 0 to 30000 loop
         START_CONV_ADC_CH_PULSE_signal <= '1';
         SDO_CH_signal(1) <= '1';
-        SDO_CH_signal(2) <= '1';
-        SDO_CH_signal(3) <= '1';
-        SDO_CH_signal(4) <= '1';
-        SDO_CH_signal(5) <= '1';
-        SDO_CH_signal(6) <= '1';
-        SDO_CH_signal(7) <= '1';
-        SDO_CH_signal(8) <= '1';
         wait for 5 ns;
+
         SDO_CH_signal(1) <= '0';
-        SDO_CH_signal(2) <= '0';
-        SDO_CH_signal(3) <= '0';
-        SDO_CH_signal(4) <= '0';
-        SDO_CH_signal(5) <= '0';
-        SDO_CH_signal(6) <= '0';
-        SDO_CH_signal(7) <= '0';
-        SDO_CH_signal(8) <= '0';
         wait for 5 ns;
+
         START_CONV_ADC_CH_PULSE_signal <= '0';
-        
         wait for 35 ns;
-        SDO_CH_signal(1) <= '0';
-        SDO_CH_signal(2) <= '0';
-        SDO_CH_signal(3) <= '0';
-        SDO_CH_signal(4) <= '0';        
-        wait for 10 ns;
-        SDO_CH_signal(1) <= '0';
-        SDO_CH_signal(2) <= '0';
-        SDO_CH_signal(3) <= '0';
-        SDO_CH_signal(4) <= '1';
-        wait for 10 ns;
-        SDO_CH_signal(1) <= '0';
-        SDO_CH_signal(2) <= '0';
-        SDO_CH_signal(3) <= '1';
-        SDO_CH_signal(4) <= '0';
-        wait for 10 ns;
-        SDO_CH_signal(1) <= '0';
-        SDO_CH_signal(2) <= '0';
-        SDO_CH_signal(3) <= '1';
-        SDO_CH_signal(4) <= '1';
-        wait for 10 ns;
-        SDO_CH_signal(1) <= '0';
-        SDO_CH_signal(2) <= '1';
-        SDO_CH_signal(3) <= '0';
-        SDO_CH_signal(4) <= '0';        
-        wait for 10 ns;
-        SDO_CH_signal(1) <= '0';
-        SDO_CH_signal(2) <= '1';
-        SDO_CH_signal(3) <= '0';
-        SDO_CH_signal(4) <= '1';        
-        wait for 10 ns;
-        SDO_CH_signal(1) <= '0';
-        SDO_CH_signal(2) <= '1';
-        SDO_CH_signal(3) <= '1';
-        SDO_CH_signal(4) <= '0';        
-        wait for 10 ns;
-        SDO_CH_signal(1) <= '0';
-        SDO_CH_signal(2) <= '1';
-        SDO_CH_signal(3) <= '1';
-        SDO_CH_signal(4) <= '1';        
-        wait for 10 ns;
+
         SDO_CH_signal(1) <= '1';
-        SDO_CH_signal(2) <= '0';
-        SDO_CH_signal(3) <= '0';
-        SDO_CH_signal(4) <= '0';        
         wait for 10 ns;
+
         SDO_CH_signal(1) <= '1';
-        SDO_CH_signal(2) <= '0';
-        SDO_CH_signal(3) <= '0';
-        SDO_CH_signal(4) <= '1';        
         wait for 10 ns;
-        SDO_CH_signal(1) <= '1';
-        SDO_CH_signal(2) <= '0';
-        SDO_CH_signal(3) <= '1';
-        SDO_CH_signal(4) <= '0';        
+
+        SDO_CH_signal(1) <= '0';
         wait for 10 ns;
+
         SDO_CH_signal(1) <= '1';
-        SDO_CH_signal(2) <= '0';
-        SDO_CH_signal(3) <= '1';
-        SDO_CH_signal(4) <= '1';        
         wait for 10 ns;
-        SDO_CH_signal(1) <= '1';
-        SDO_CH_signal(2) <= '1';
-        SDO_CH_signal(3) <= '0';
-        SDO_CH_signal(4) <= '0';        
+
+        SDO_CH_signal(1) <= '0';
         wait for 10 ns;
+
         SDO_CH_signal(1) <= '1';
-        SDO_CH_signal(2) <= '1';
-        SDO_CH_signal(3) <= '0';
-        SDO_CH_signal(4) <= '1';        
         wait for 10 ns;
+
         SDO_CH_signal(1) <= '1';
-        SDO_CH_signal(2) <= '1';
-        SDO_CH_signal(3) <= '1';
-        SDO_CH_signal(4) <= '0';        
         wait for 10 ns;
+
         SDO_CH_signal(1) <= '1';
-        SDO_CH_signal(2) <= '1';
-        SDO_CH_signal(3) <= '1';
-        SDO_CH_signal(4) <= '1';
+        wait for 10 ns;
+
+        SDO_CH_signal(1) <= '0';
+        wait for 10 ns;
+
+        SDO_CH_signal(1) <= '0';
+        wait for 10 ns;
+
+        SDO_CH_signal(1) <= '0';
+        wait for 10 ns;
+
+        SDO_CH_signal(1) <= '0';
+        wait for 10 ns;
+
+        SDO_CH_signal(1) <= '1';
+        wait for 10 ns;
+
+        SDO_CH_signal(1) <= '1';
+        wait for 10 ns;
+
+        SDO_CH_signal(1) <= '0';
+        wait for 10 ns;
+
+        SDO_CH_signal(1) <= '1';
         wait for 5 ns;
+
 --        START_CONV_ADC_CH_PULSE_signal <= '1';
-        
     end loop;
 
 END PROCESS ADC_CH_conv_trigger;
@@ -334,15 +289,15 @@ END PROCESS DAC_CH_conv_trigger;
 -----------------------------------------------------------------------------------------------------------------------		
 rst: PROCESS
 BEGIN
-	rst_signal <= '1';
-	wait for 1000 ns;
-	rst_signal <= '0';
-		 wait for 50 ms;
-		 rst_signal <= '1';
-		 wait for 1000 ns;
-		 rst_signal <= '0';
-	wait for 2000 ms;
-	
+    rst_signal <= '1';
+    wait for 1000 ns;
+    rst_signal <= '0';
+         wait for 50 ms;
+         rst_signal <= '1';
+         wait for 1000 ns;
+         rst_signal <= '0';
+    wait for 2000 ms;
+    
 END PROCESS rst;
 
 -----------------------------------------------------------------------------------------------------------------------												
@@ -350,10 +305,10 @@ END PROCESS rst;
 -----------------------------------------------------------------------------------------------------------------------		
 clk100mhz : PROCESS
 BEGIN
-	clk_signal_100mhz <= '1';
-	wait for Tclk_halfperiod_100mhz;
-	clk_signal_100mhz <= '0';
-	wait for Tclk_halfperiod_100mhz;
+    clk_signal_100mhz <= '1';
+    wait for Tclk_halfperiod_100mhz;
+    clk_signal_100mhz <= '0';
+    wait for Tclk_halfperiod_100mhz;
 END PROCESS clk100mhz;
 
 
--- src/UART_TX_CTRL.vhd
+++ src/UART_TX_CTRL.vhd
@@ -78,79 +78,79 @@ begin
 --Next state logic
 next_txState_process : process (CLK)
 begin
-	if (rising_edge(CLK)) then
-		case txState is 
-		when RDY =>
-			if (SEND = '1') then
-				txState <= LOAD_BIT;
-			end if;
-		when LOAD_BIT =>
-			txState <= SEND_BIT;
-		when SEND_BIT =>
-			if (bitDone = '1') then
-				if (bitIndex = BIT_INDEX_MAX) then
-					txState <= RDY;
-				else
-					txState <= LOAD_BIT;
-				end if;
-			end if;
-		when others=> --should never be reached
-			txState <= RDY;
-		end case;
-	end if;
+    if (rising_edge(CLK)) then
+        case txState is 
+        when RDY =>
+            if (SEND = '1') then
+                txState <= LOAD_BIT;
+            end if;
+        when LOAD_BIT =>
+            txState <= SEND_BIT;
+        when SEND_BIT =>
+            if (bitDone = '1') then
+                if (bitIndex = BIT_INDEX_MAX) then
+                    txState <= RDY;
+                else
+                    txState <= LOAD_BIT;
+                end if;
+            end if;
+        when others=> --should never be reached
+            txState <= RDY;
+        end case;
+    end if;
 end process;
 
 bit_timing_process : process (CLK)
 begin
-	if (rising_edge(CLK)) then
-		if (txState = RDY) then
-			bitTmr <= (others => '0');
-		else
-			if (bitDone = '1') then
-				bitTmr <= (others => '0');
-			else
-				bitTmr <= bitTmr + 1;
-			end if;
-		end if;
-	end if;
+    if (rising_edge(CLK)) then
+        if (txState = RDY) then
+            bitTmr <= (others => '0');
+        else
+            if (bitDone = '1') then
+                bitTmr <= (others => '0');
+            else
+                bitTmr <= bitTmr + 1;
+            end if;
+        end if;
+    end if;
 end process;
 
 bitDone <= '1' when (bitTmr = BIT_TMR_MAX) else
-				'0';
+                '0';
 
 bit_counting_process : process (CLK)
 begin
-	if (rising_edge(CLK)) then
-		if (txState = RDY) then
-			bitIndex <= 0;
-		elsif (txState = LOAD_BIT) then
-			bitIndex <= bitIndex + 1;
-		end if;
-	end if;
+    if (rising_edge(CLK)) then
+        if (txState = RDY) then
+            bitIndex <= 0;
+        elsif (txState = LOAD_BIT) then
+            bitIndex <= bitIndex + 1;
+        end if;
+    end if;
 end process;
 
 tx_data_latch_process : process (CLK)
 begin
-	if (rising_edge(CLK)) then
-		if (SEND = '1') then
-			txData <= '1' & DATA & '0';
-		end if;
-	end if;
+    if (rising_edge(CLK)) then
+        if (SEND = '1') then
+            txData <= '1' & DATA & '0';
+        end if;
+    end if;
 end process;
 
 tx_bit_process : process (CLK)
 begin
-	if (rising_edge(CLK)) then
-		if (txState = RDY) then
-			txBit <= '1';
-		elsif (txState = LOAD_BIT) then
-			txBit <= txData(bitIndex);
-		end if;
-	end if;
+    if (rising_edge(CLK)) then
+        if (txState = RDY) then
+            txBit <= '1';
+        elsif (txState = LOAD_BIT) then
+            txBit <= txData(bitIndex);
+        end if;
+    end if;
 end process;
 
 UART_TX <= txBit;
 READY <= '1' when (txState = RDY) else
-			'0';
+            '0';
 
 end Behavioral;
