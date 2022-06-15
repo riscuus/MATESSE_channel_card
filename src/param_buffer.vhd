3247abd93f5e2c53fa44ed2139618d084a808052
riscuus <riscoalbert@gmail.com>
2022-06-15 16:59:39 -0400

    Added input to be able to set default values

--- src/param_buffer.vhd
+++ src/param_buffer.vhd
@@ -28,38 +28,36 @@ use concept.utils.all;
 
 entity param_buffer is
     generic(
-        param_size              : natural; -- Number of words that the param value occupies
-        address                 : natural  -- The address of this parameter
+        param_size          : natural; -- Number of words that the param value occupies
+        param_id            : natural  -- The id of this parameter
     );
     port(
-        clk             : in std_logic; -- 5mhz clock
-        rst             : in std_logic; -- asynchronous reset
+        clk                 : in std_logic; -- 5mhz clock
+        rst                 : in std_logic; -- asynchronous reset
 
-        update          : in std_logic; -- Pulse that is broadcasted indicate that the value has to be updated
-        update_address  : in natural;   -- The address of the parameter that has to be updated
-        update_data     : in t_packet_payload; -- The data to be updated
-        param_data      : out t_param_array(0 to param_size - 1) -- The current data being provided to the connected block
+        update              : in std_logic; -- Pulse that is broadcasted indicate that the value has to be updated
+        param_id_to_update  : in natural;   -- The id of the parameter that has to be updated
+        update_data         : in t_packet_payload; -- The data to be updated
+        default_value       : in t_param_array(0 to param_size - 1); -- The initial value that this param should have
+        param_data          : out t_param_array(0 to param_size - 1) -- The current data being provided to the connected block
     );
 
 end param_buffer;
 
 architecture behave of param_buffer is
 
-    signal param_data_reg : t_param_array(0 to param_size - 1) := (others => (others => '0'));
-
 begin
 
-    param_data <= param_data_reg;
 
 main_logic : process(clk, rst)
 begin
     if (rst = '1') then
-        param_data_reg <= (others => (others => '0'));
+        param_data <= default_value;
 
     elsif (rising_edge(clk)) then
-        if (update = '1' and update_address = address) then
-            for i in param_data'range loop
-                param_data_reg(i) <= update_data(i);
+        if (update = '1' and param_id_to_update = param_id) then
+            for i   in param_data'range loop
+                param_data(i) <= update_data(i);
             end loop;
         end if;
     end if;
